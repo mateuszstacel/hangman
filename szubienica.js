cc23443caff55f40c76fbe0d1ec1867daf40521a
mateuszstacel <mateusz.stacel92@gmail.com>
2018-11-25 13:49:57 +0000

    checking if number is click.

--- szubienica.js
+++ szubienica.js
@@ -19,6 +19,41 @@ function wypisz_haslo()
 window.onload = start;
 
 var litery = new Array(35);
+litery[0] = "A";
+litery[1] = "Ą";
+litery[2] = "B";
+litery[3] = "C";
+litery[4] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ż";
+litery[34] = "Ź";
 
 function start()
 {
@@ -27,12 +62,27 @@ function start()
 
   for(i=0; i<=34; i++)
   {
-    alfabet = alfabet + '<div class ="litera">A</div>';
+    var element = "lit" + i;
+    alfabet = alfabet + '<div class ="litera" onclick="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>';
     if((i+1)%7==0) alfabet = alfabet+'<div style="clear:both;"></div>';
   }
 
 
   document.getElementById('alfabet').innerHTML = alfabet;
 
+  wypisz_haslo()
+}
+
+String.prototype.ustawZnak = function(miejsce, znak){
+  if (miejsce > this.length - 1) return this.toString();
+  else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+}
+
+function sprawdz(nr) {
+  for(i=0; i<dlugosc_hasla; i++){
+    if(haslo.charAt(i)== litery[nr]){
+      haslo1 = haslo1.ustawZnak.(i,litery[nr]);
+    }
+  }
   wypisz_haslo()
 }
